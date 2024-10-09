1d31ae5815c362a8115ed54b035a871b22ab23c1
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2024-10-09 10:34:55 -0300

    Inital commit

---
+++ GeniusQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeniusQR
+//
+//  Created by Matheus  Torres on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
